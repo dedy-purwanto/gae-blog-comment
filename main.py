e3eda76c0c6bdb53b3d21d3146abd55c26ce4718
Dedi Purwanto <kecebongsoft@gmail.com>
2014-03-13 13:06:17 +0800

    Store to database

--- main.py
+++ main.py
@@ -16,15 +16,57 @@
 #
 import webapp2
 import json
+from datetime import datetime
+from google.appengine.ext import db
+
+class Comment(db.Model):
+    url = db.StringProperty(required=True)
+    name = db.StringProperty(required=True)
+    comment = db.StringProperty(required=True)
+    date = db.DateTimeProperty(required=True)
+
+class PostCommentHandler(webapp2.RequestHandler):
+    def dispatch(self):
+        self.response.headers['Access-Control-Allow-Origin'] = '*'
+        super(PostCommentHandler, self).dispatch()
+
+    def get(self):
+        name = self.request.get('name');
+        comment = self.request.get('comment');
+        url = self.request.get('url');
+        if name and comment and len(name) > 0 and len(comment) > 0:
+            c = Comment(url=url, name=name, comment=comment, date=datetime.now())
+            c.put()
+
+            new_comment = {'name': name, 'comment': comment, 'date': c.date.__str__()}
+            self.response.headers['Access-Control-Allow-Origin'] = '*'
+            self.response.write(json.dumps(new_comment))
+
 
 class GetCommentsHandler(webapp2.RequestHandler):
+    def get(self):
+        comments = {'comments':[]}
+        for c in db.GqlQuery("SELECT * FROM Comment WHERE url = :1 ORDER BY date DESC", self.request.get('url')):
+            comment = c.comment
+            comment = comment.replace('\n', '<br />')
+            comment = comment.replace('\r', '<br />')
+            comment = comment.replace('\n\r', '<br />')
 
+            comments['comments'].append({
+                'name': c.name,
+                'comment': comment,
+                'date': c.date.__str__(),
+            })
 
+        self.response.headers['Access-Control-Allow-Origin'] = '*'
+        self.response.write(json.dumps(comments))
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello world!')
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/get/', GetCommentsHandler),
+    ('/post/', PostCommentHandler),
+    ('/', MainHandler),
 ], debug=True)
